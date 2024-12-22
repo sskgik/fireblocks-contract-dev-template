cee8ef2828545cba2a3c4cf10e96e187450c4d20
sskgik <sskgik19@gmail.com>
2024-12-22 21:09:45 +0900

    first commit

--- contracts/verify/Token.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
-
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract Token is ERC20 {
-  constructor() ERC20("TestToken", "TEST") {}
-}
